194a5da271da702a7a16b2532a98c3d397c8f775
Sebastián Aburto <seba.aburto@duocuc.cl>
2025-11-28 03:56:04 -0300

    Microservicio_InventarioPMS_V4

---
+++ Microservicio_InventarioPMS/Microservicio_InventarioPMS/src/main/java/com/example/Microservicio_InventarioPMS/MicroservicioInventarioPmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservicio_InventarioPMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioInventarioPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioInventarioPmsApplication.class, args);
+	}
+
+}
